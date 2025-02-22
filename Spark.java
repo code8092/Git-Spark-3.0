46be7a54905a83c2ec33e363ce13026e5c6cc63c
RAJ ANAND <anandraj82698@gmail.com>
2025-02-22 08:50:15 +0530

    added notify

--- Spark.java
+++ Spark.java
@@ -1,6 +1,9 @@
 class Spark{
-    public static void m1(String[] args) {
+    public static void payment() {
         System.out.println("m1");
+    }
+    public static void notifyUser() {
+        System.out.println("Hello User");
     }
     public static void main(String[] args){
         System.out.println("Hello");
