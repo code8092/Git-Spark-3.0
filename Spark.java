969b6fcab1d9e238b229c547e2e31691fed4697d
RAJ ANAND <anandraj82698@gmail.com>
2025-02-22 08:37:05 +0530

    Change in spark file

--- Spark.java
+++ Spark.java
@@ -1,4 +1,7 @@
 class Spark{
+    public static void m1(String[] args) {
+        System.out.println("m1");
+    }
     public static void main(String[] args){
         System.out.println("Hello");
     }
